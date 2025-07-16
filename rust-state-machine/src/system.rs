aa3d7e03b3b1ab6786208390f5178e86a450f2aa
Rama Baliola <rama@mandalachain.io>
2025-07-16 13:27:26 +0800

    fix(system): fix struct in pallet runtime

--- rust-state-machine/src/system.rs
+++ rust-state-machine/src/system.rs
@@ -3,10 +3,13 @@ use std::collections::BTreeMap;
 pub struct Pallet {
 	block_number: u32,
 
-	nonce: BTreeMap<String, u32>,
+	pub(crate) nonce: BTreeMap<String, u32>,
 }
 
 impl Pallet {
+	pub fn new() -> Self {
+		Self { block_number: 0, nonce: BTreeMap::new() }
+	}
 	/// Get the current block number.
 	pub fn block_number(&self) -> u32 {
 		self.block_number
