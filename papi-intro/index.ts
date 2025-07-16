9901bcc96bbd1f625ad2338938877131a794c4f6
Rama Baliola <rama@mandalachain.io>
2025-07-16 09:38:02 +0800

    feat(papi): finish papi intro

--- papi-intro/index.ts
+++ papi-intro/index.ts
@@ -4,12 +4,18 @@ import {
   type PolkadotClient,
   type SS58String,
 } from "polkadot-api";
-import { dot, people } from "@polkadot-api/descriptors";
+import { dot, people, collectives } from "@polkadot-api/descriptors";
 
 const RPC_ENDPOINT = "wss://rpc.polkadot.io";
 const PEOPLE_RPC_ENDPOINT = "wss://polkadot-people-rpc.polkadot.io";
+const COLLECTIVES_RPC_ENDPOINT = "wss://polkadot-collectives-rpc.polkadot.io";
+const WALLET_ADDRESS = "15DCZocYEM2ThYCAj22QE4QENRvUNVrDtoLBVbCm5x4EQncr";
+
+interface FellowshipMember {
+  address: SS58String;
+  rank: number;
+}
 function makeClient(endpoint: string): PolkadotClient {
-  console.log("--------------------------------------");
   console.log("Creating Polkadot client...");
   console.log(`Connecting to endpoint: ${endpoint}`);
   console.log("--------------------------------------");
@@ -22,7 +28,6 @@ function makeClient(endpoint: string): PolkadotClient {
 async function printChainInfo(client: PolkadotClient) {
   let chain = await client.getChainSpecData();
   let finalizedBlock = await client.getFinalizedBlock();
-  console.log("--------------------------------------");
   console.log(
     `Connected to ${chain.name} at block ${finalizedBlock.number}.\n`
   );
@@ -38,6 +43,18 @@ async function getBalance(
   return free + reserved;
 }
 
+async function getFellowshipMembers(
+  collectivesClient: PolkadotClient
+): Promise<FellowshipMember[]> {
+  const collectivesApi = collectivesClient.getTypedApi(collectives);
+  const rawMembers =
+    await collectivesApi.query.FellowshipCollective.Members.getEntries();
+  const data = rawMembers.map((m) => {
+    return { address: m.keyArgs[0], rank: m.value };
+  });
+  return data;
+}
+
 async function getDisplayName(
   peopleClient: PolkadotClient,
   address: SS58String
@@ -51,18 +68,28 @@ async function getDisplayName(
 }
 
 async function main() {
-  const walletAddress = "15DCZocYEM2ThYCAj22QE4QENRvUNVrDtoLBVbCm5x4EQncr";
   const polkadotClient = makeClient(RPC_ENDPOINT);
   const peopleClient = makeClient(PEOPLE_RPC_ENDPOINT);
   await printChainInfo(peopleClient);
-  const balance = await getBalance(polkadotClient, walletAddress);
-  console.log("--------------------------------------");
-  console.log("Client created successfully.");
-  console.log("--------------------------------------");
-  const displayName = await getDisplayName(peopleClient, walletAddress);
-  console.log(`Balance of ${displayName} (${walletAddress}) is ${balance}.`);
-  console.log("--------------------------------------");
-  printChainInfo(polkadotClient);
+  await printChainInfo(polkadotClient);
+  const collectivesClient = makeClient(COLLECTIVES_RPC_ENDPOINT);
+  await printChainInfo(collectivesClient);
+
+  const members = await getFellowshipMembers(collectivesClient);
+
+  console.log("Generating table...");
+  const table = [];
+  for (const { address, rank } of members) {
+    const balance = await getBalance(polkadotClient, address);
+    const displayName = await getDisplayName(peopleClient, address);
+    table.push({ rank, displayName, address, balance });
+    // console.log(`Balance of ${displayName} (${WALLET_ADDRESS}) is ${balance}.`);
+  }
+  table.sort((a, b) => b.rank - a.rank);
+  console.table(table);
+
+  console.log(`Done!`);
+  process.exit(0);
 }
 
 main().catch((error) => {
